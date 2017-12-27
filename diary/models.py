01007fc7e2df41885fb9ac0065c18578777a7233
장지용 <jangjiyong@jangjiyong-ui-MacBook-Pro.local>
2017-12-27 23:05:57 +0900

    RDS 연결 설정 추가

--- diary/models.py
+++ diary/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=1024)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
-    name = models.CharField(max_length=1024,unique=True)
+    name = models.CharField(max_length=50,unique=True)
     
     def __str__(self):
         return self.name
 
 class Note(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
-    title = models.CharField(max_length=1024)
+    title = models.CharField(max_length=255)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -27,7 +27,7 @@ class Note(models.Model):
         ordering = ['id']
 
 class Diary(models.Model):
-    title = models.CharField(max_length=1024)
+    title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
